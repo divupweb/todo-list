3adee00ddd7daeb79c64b7c89a87ccd074e6db30
divupweb <divupwebgamepoe@gmail.com>
2022-02-01 12:35:55 +0300

    added layout

--- src/components/header.components.js
+++ src/components/header.components.js
@@ -1,18 +1,17 @@
-import {Component} from '../core/component'
-export class HeaderComponents extends Component{
-    constructor(id){
-        super(id);
-        this.init()
-    }
-    init = () =>{
-        const $BTN = this.$element.querySelector('.js-header-start');
-        localStorage.getItem('privacy') ? this.hide() : null;
-        $BTN.addEventListener('click',buttonHandler.bind(this));
-    }
+import { Component } from "../core/component";
+export class HeaderComponents extends Component {
+  constructor(id) {
+    super(id);
+    this.init();
+  }
+  init = () => {
+    const $BTN = this.$element.querySelector(".js-header-start");
+    localStorage.getItem("privacy") ? this.hide(true) : null;
+    $BTN.addEventListener("click", buttonHandler.bind(this));
+  };
 }
 
-
 function buttonHandler() {
-    this.hide();
-    localStorage.setItem('privacy',true);
+  this.hide();
+  localStorage.setItem("privacy", true);
 }
--- src/core/component.js
+++ src/core/component.js
@@ -1,13 +1,20 @@
-export class Component{
-    constructor(id){
-        this.$element = document.getElementById(id);
-        this.init();
+export class Component {
+  constructor(id) {
+    this.$element = document.getElementById(id);
+    this.init();
+  }
+  init = () => {};
+  hide = (trigger) => {
+    if (trigger) {
+      this.$element.classList.add("shared__section-hide");
+    } else {
+      this.$element.classList.add("shared__section-fade-out");
+      setTimeout(() => {
+        this.$element.classList.add("shared__section-hide");
+      }, 2000);
     }
-    init = () =>{}
-    hide = () =>{
-        this.$element.classList.add('shared__section-hide');
-    }
-    show = () =>{
-        this.$element.classList.remove('shared__section-hide');
-    }
+  };
+  show = () => {
+    this.$element.classList.remove("shared__section-hide");
+  };
 }
